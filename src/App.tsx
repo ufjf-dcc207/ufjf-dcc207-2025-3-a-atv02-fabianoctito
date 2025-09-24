1ee53e60ee6522618ed549a8cbcbce90b66ae957
Fabiano <fabiano.camara@estudante.ufjf.br>
2025-09-23 22:12:31 -0300

    ✨ Adicionado componente Disciplina

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import './App.css'
+import Disciplina from './Disciplina';
 
 function App() {
   return (
     <>
     <h1>Ola mundo!!!</h1>
     <h2>Ola mundo!!!</h2>
+    <Disciplina></Disciplina>
     </>
   );
 }
---
+++ src/Disciplina.tsx
@@ -0,0 +1,7 @@
+export default function Disciplina(){
+    return (<>
+        <h1>DCC207</h1>
+        <h2>Desenvolvimento Web Front End</h2>
+    </>)
+}
+
